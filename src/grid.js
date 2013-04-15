742f148e7c232332ca295948a605b796fb5061ed
stonelee <istonelee@gmail.com>
2013-04-14 19:07:57 -0800

    Update documentation

--- src/grid.js
+++ src/grid.js
@@ -8,80 +8,159 @@ define(function(require, exports, module) {
 
   var Grid = Widget.extend({
     attrs: {
-      //行默认高度
-      rowHeight: 23
-    },
-    events: {
-      'click .grid-hd': 'sort',
-      'click .grid-row': 'click',
-      'click :not(.icon-btn-is-disabled)[data-role=prev]': 'prevPage',
-      'click :not(.icon-btn-is-disabled)[data-role=next]': 'nextPage',
-      'click :not(.icon-btn-is-disabled)[data-role=first]': 'firstPage',
-      'click :not(.icon-btn-is-disabled)[data-role=last]': 'lastPage',
-      'click [data-role=refresh]': 'refresh',
-      'keyup [data-role=num]': 'gotoPage'
+      title: '',
+      url: '',
+      urlParser: null,
+      data: [],
+      fields: [],
+      width: 0,
+      height: 0
     },
 
-    sort: function(e) {
-      var cell = $(e.target).closest('td');
+    _onRenderUrl: function(url) {
+      var self = this;
+      $.getJSON(url, function(data) {
+        self._createGrid(data.data);
+      });
+    },
+    _onRenderData: function(data) {
+      this._createGrid(data);
+    },
+
+    _createGrid: function(data) {
+      this.data = data;
+
+      var gridWidth = this.get('width') || this.element.parent().width();
+      var fields = this._processField(gridWidth);
+      var records = $.map(data.result, function(record, index) {
+        return {
+          isAlt: index % 2 === 1,
+          id: record.id,
+          values: $.map(fields, function(field) {
+            var value = record[field.name];
+            value = _.escape(value);
+
+            if ($.isFunction(field.render)) {
+              value = field.render(value);
+            }
+
+            return {
+              width: field.width,
+              align: field.align,
+              value: value
+            };
+          })
+        };
+      });
+
+      var gridHeight = this.get('height');
+      var html = handlebars.compile(tpl)({
+        width: gridWidth,
+        title: this.get('title'),
+        height: gridHeight,
+        fields: fields,
+        records: records,
+        isFirst: function() {
+          return data.pageNumber <= 1;
+        },
+        isLast: function() {
+          return data.totalPages === 0 || data.pageNumber === data.totalPages;
+        },
+        hasPrev: data.hasPrev,
+        hasNext: data.hasNext,
+        totalCount: data.totalCount,
+        pageSize: data.pageSize
+      });
+      this.element.html(html);
+
+      if (!gridHeight) {
+        gridHeight = this.element.height() - this.$('.grid-bd').position().top - this.$('.toolbar-ft').outerHeight() - 1;
+        this.$('.grid-bd').height(gridHeight);
+      }
+
+      this.$('[data-role=num]').val(data.pageNumber);
+
+      //disabled button will not be clicked
+      this.$('.icon-btn').click(function(e) {
+        if ($(this).hasClass('icon-btn-is-disabled')) {
+          e.stopImmediatePropagation();
+        }
+      });
+    },
+    _processField: function(gridWidth) {
+      var fields = this.get('fields');
+
+      var totalWidth = 0,
+        totalNum = 0;
+      $.each(fields, function() {
+        if (this.width) {
+          totalWidth += this.width;
+          totalNum += 1;
+        }
+      });
+
+      var averageWidth = (gridWidth - fields.length * 9 - totalWidth - 18) / (fields.length - totalNum);
+
+      fields = $.map(fields, function(field) {
+        if (!field.width) {
+          field.width = averageWidth;
+        }
+        return field;
+      });
+      return fields;
+    },
+
+    events: {
+      'click .grid-hd': '_sort',
+      'click .grid-row': '_click',
+      'click [data-role=prev]': 'prevPage',
+      'click [data-role=next]': 'nextPage',
+      'click [data-role=first]': 'firstPage',
+      'click [data-role=last]': 'lastPage',
+      'click [data-role=refresh]': 'refresh',
+      'keyup [data-role=num]': '_gotoPage'
+    },
+
+    _sort: function(e) {
+      var cell = $(e.target).closest('th');
       var name = cell.attr('data-name');
 
       //只能按照单独的列排序
-      if (!this.oldSortHeader) {
-        this.oldSortHeader = cell;
+      if (!this._oldSortHeader) {
+        this._oldSortHeader = cell;
       } else {
-        if (this.oldSortHeader.attr('data-name') !== name) {
-          this.oldSortHeader.removeClass('grid-hd-is-desc grid-hd-is-asc');
-          this.oldSortHeader = cell;
+        if (this._oldSortHeader.attr('data-name') !== name) {
+          this._oldSortHeader.removeClass('grid-is-desc grid-is-asc');
+          this._oldSortHeader = cell;
         }
       }
 
-      if (cell.hasClass('grid-hd-is-desc')) {
-        cell.removeClass('grid-hd-is-desc').addClass('grid-hd-is-asc');
-        console.log(name, 'asc');
+      if (cell.hasClass('grid-is-desc')) {
+        cell.removeClass('grid-is-desc').addClass('grid-is-asc');
+        this.trigger('sort', name, 'asc');
       } else {
-        cell.removeClass('grid-hd-is-asc').addClass('grid-hd-is-desc');
-        console.log(name, 'desc');
+        cell.removeClass('grid-is-asc').addClass('grid-is-desc');
+        this.trigger('sort', name, 'desc');
       }
     },
 
-    click: function(e) {
-      var cell = $(e.target);
-      var row = cell.parents('tr');
+    _click: function(e) {
+      var target = $(e.target);
+      var row = target.parents('tr');
 
       var id = row.attr('data-id');
       var data = _.find(this.data.result, function(record) {
-        return record.id = id;
+        return record.id == id;
       });
-      this.trigger('click', data, cell, row);
+      this.trigger('click', target, data);
     },
 
-    prevPage: function() {
-      var id = this.data.prevPage;
-      this.fetch(id);
-    },
-    nextPage: function() {
-      var id = this.data.nextPage;
-      this.fetch(id);
-    },
-    firstPage: function() {
-      var id = this.data.firstPage;
-      this.fetch(id);
-    },
-    lastPage: function() {
-      var id = this.data.lastPage;
-      this.fetch(id);
-    },
-    refresh: function() {
-      var id = this.data.pageNumber;
-      this.fetch(id);
-    },
-    gotoPage: function(e) {
+    _gotoPage: function(e) {
       var $input = $(e.target);
       var value = $input.val();
 
       if (value && e.which == 13) {
-        this.fetch(value);
+        this.gotoPage(value);
       } else {
         value = value.replace(/\D/g, '');
         if (value) {
@@ -100,84 +179,32 @@ define(function(require, exports, module) {
       }
     },
 
-    fetch: function(id) {
-      var that = this;
-      var url = this.urlFormat(id);
-      $.getJSON(url, function(data) {
-        that._createGrid(data.data);
-      });
+    //public method
+    gotoPage: function(id) {
+      var r = this.get('urlParser');
+      var url = this.get('url').replace(r, '$1' + id + '$2');
+      this.set('url', url);
     },
-
-    urlFormat: function(id) {
-      return id;
+    prevPage: function() {
+      var id = this.data.prevPage;
+      this.gotoPage(id);
     },
-
-    setup: function() {
-      var that = this;
-
-      Grid.superclass.setup.call(this);
-
+    nextPage: function() {
+      var id = this.data.nextPage;
+      this.gotoPage(id);
+    },
+    firstPage: function() {
+      var id = this.data.firstPage;
+      this.gotoPage(id);
+    },
+    lastPage: function() {
+      var id = this.data.lastPage;
+      this.gotoPage(id);
+    },
+    refresh: function() {
+      //刷新往往不会改变url
       var url = this.get('url');
-      if (url) {
-        $.getJSON(url, function(data) {
-          that._createGrid(data.data);
-        });
-      } else {
-        //避免向服务端发送请求
-        var data = this.get('data');
-        if (data) {
-          this._createGrid(data);
-        }
-      }
-
-    },
-    _createGrid: function(data) {
-      this.data = data;
-
-      var title = this.get('title');
-      var fields = this.get('fields');
-      var records = $.map(data.result, function(record, index) {
-        return {
-          isAlt: index % 2 === 1,
-          id: record.id,
-          values: $.map(fields, function(field) {
-            var value = record[field.name];
-            value = _.escape(value);
-
-            if ($.isFunction(field.render)) {
-              return field.render(value);
-            } else {
-              return value;
-            }
-          })
-        };
-      });
-
-      var html = handlebars.compile(tpl)({
-        title: title,
-        fields: fields,
-        records: records,
-        isFirst: function() {
-          return data.pageNumber <= 1;
-        },
-        isLast: function() {
-          return data.totalPages === 0 || data.pageNumber === data.totalPages;
-        },
-        hasPrev: data.hasPrev,
-        hasNext: data.hasNext,
-        totalCount: data.totalCount,
-        pageSize: data.pageSize
-      });
-      this.element.html(html);
-
-      this.$('[data-role=num]').val(data.pageNumber);
-
-      this._fixFooterPosition();
-    },
-
-    _fixFooterPosition: function() {
-      var blankHeight = this.get('rowHeight') * (this.data.pageSize - this.data.result.length);
-      this.$('.grid-ft').css('margin-top', blankHeight);
+      this._onRenderUrl(url);
     }
 
   });
